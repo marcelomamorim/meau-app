6fea9c14630746b238f449b5fe932a9607f7dd45
Marcelo <marceloamorim.webdev@gmail.com>
2024-07-15 00:26:44 -0300

    ajuste pipeline - config

--- frontend/config/config.tsx
+++ frontend/config/config.tsx
@@ -4,13 +4,6 @@ import {getAuth} from "firebase/auth";
 import {getFirestore} from "@firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCT81l4DgQ9aLZaqI1gkeNcWMLydbgfGVU",
-    authDomain: "meau-app-fda38.firebaseapp.com",
-    projectId: "meau-app-fda38",
-    storageBucket: "meau-app-fda38.appspot.com",
-    messagingSenderId: "655805003245",
-    appId: "1:655805003245:web:a6310ea2638c1cf8a4a3e4",
-    measurementId: "G-BBR53BFX4P"
   };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
