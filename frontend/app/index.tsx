4e143367cc1c1cba61c5bbcce7642d850fb7a37a
igorbastos <igor.oliveira@singletech.com.br>
2024-08-03 17:10:29 -0300

    teste apk: ele ja  bulda mas ainda ocorre alguns problemas

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,9 +1,14 @@
 import React, {useEffect} from 'react';
 import SplashScreen from "@/screens/Splash/SplashScreen";
+import {FIREBASE_APP} from "@/config/config";
 import "firebase/firestore";
 
 
 const App = () => {
+
+    useEffect(() => {
+        const app = FIREBASE_APP;
+    }, []);
 
     return (
         <SplashScreen>
--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -4,7 +4,15 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
-      '@babel/plugin-transform-template-literals'
+      '@babel/plugin-transform-template-literals',
+      ['module:react-native-dotenv', {
+        "moduleName": "@env",
+        "path": ".env",
+        "blacklist": null,
+        "whitelist": null,
+        "safe": false,
+        "allowUndefined": true
+      }]
     ],
   };
 };
